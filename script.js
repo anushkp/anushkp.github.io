724abb6d5a1013d89f19c5d8681383746c8cad59
Anush Gowda K P <anushkpkp@gmail.com>
2026-07-24 10:21:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyProduct(product){
+    alert(product + " added to cart!");
+}
+
+function sendMessage(event){
+    event.preventDefault();
+    alert("Thank you! Your message has been received.");
+}
